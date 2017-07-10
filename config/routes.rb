87d91f81cdf8ba73e06ab7e509410e54c88ed66c
Romain BOBOE <rboboe@gmail.com>
2017-07-10 17:04:31 -0400

    creating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :products do
+    resources :reviews, only: [:show, :new, :create]
+    resources :reservations
+  end
+    resources :users, except: [:index, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
