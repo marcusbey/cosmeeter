cedaa0375f6f14ee60a6b73ac8028579f53a755a
Jean-Sebastien Dufresne <jsdufresne@iciela.org>
2017-07-10 18:08:51 -0400

    Review controller created

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,23 @@
+class ReviewsController < ApplicationController
+  def create
+    @reservation = Reservation.find(params[:reservation_id])
+    @user = User.find(params[:user_id])
+    @review = Review.new()
+    @review.content = review_params['content']
+    @review.rating = review_params['rating']
+    @review.user = User.find(review_params[:user_id])
+    @review.reservation = Reservation.find(params[:reservation_id])
+    if @review.save!
+      redirect_to reservation_path(@reservation)
+    else
+      render :new
+    end
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
