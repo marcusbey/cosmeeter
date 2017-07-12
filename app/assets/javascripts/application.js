99f0f78a1c1ceeb679cbaf5af132c8911c058062
Milan Barande <milan.barande@hotmail.fr>
2017-07-12 15:16:10 -0400

    updated reservation and javascript file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,9 @@
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
+//= require materialize
+//= require materialize/extras/nouislider
+//= require_tree .
+
