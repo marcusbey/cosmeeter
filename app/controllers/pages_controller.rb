4af2ead981fd05da753067133f55132a9d6e6526
Romain BOBOE <rboboe@gmail.com>
2017-07-13 16:12:14 -0400

    redirection to products page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
+  before_action :redirect_if_logged_in, only: [:home]
 
   def home
+  end
+
+  private
+
+  def redirect_if_logged_in
+    redirect_to products_path if user_signed_in?
   end
 end
